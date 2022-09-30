94fa507525326be574182c53bfa690dd73fd36b2
DarshanKansara504 <109453833+DarshanKansara504@users.noreply.github.com>
2022-10-01 01:16:33 +0530

    aws encapsulation

--- UserServiceImpl.java
+++ UserServiceImpl.java
@@ -36,6 +36,21 @@ public class UserServiceImpl implements UserService {
 
 	// getting user by username
 	@Override
+	int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
 	public User getUser(String username) throws Exception {
 		User local = this.userRepository.findByUsername(username);
 
